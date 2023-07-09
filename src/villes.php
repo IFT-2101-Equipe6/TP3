9cf0e16faf4e92d1a3de195cb8865333cbb90486
root <root@localhost.localdomain>
2023-07-09 19:29:33 -0400

    fix: server now runs; installed mysqli

--- src/villes.php
+++ src/villes.php
@@ -4,18 +4,39 @@
 </head>
 
 <body>
-    <h1>Équipe 6</h1>
-
     <?php
+        $servername = 'bd';
         $dbName = 'equipe6';
         $dbuser = 'root';
         $dbpassword = 'password';
-        $dbhost = '127.0.0.1';
+        $dbhost = '172.25.0.2';
         $dbPort = '3306';
 
         $connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbName, $dbPort);
-
-        // $result = mysqli_query($connection, "SELECT * FROM villes");
+        
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+        
+        $sql = "SELECT nom, region, population FROM villes";
+        $result = mysqli_query($connection, $sql);
+    
+        if ($result->num_rows > 0) {
+            echo "<h1>Équipe 6</h1>";
+            echo "<table>";
+            echo "<tr><th>Nom</th><th>Région</th><th>Population</th></tr>";
+            
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>" . $row["nom"] . "</td><td>" . $row["region"] . "</td><td>" . number_format($row["population"], 0, ',', ' ') . "</td></tr>";
+            }
+            
+            echo "</table>";
+        } else {
+            echo "Aucune ville trouvée.";
+        }
+        
+        $connection->close();
     ?>
+    
 </body>
 </html>
